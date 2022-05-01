9416367463826ccf9fc5745a3f7689439e17276d
unknown <mdtauhidulislam509@gmail.com>
2022-05-01 23:05:16 +0600

    delete item

--- index.js
+++ index.js
@@ -55,6 +55,14 @@ async function run(){
         res.send(result);
       })
 
+       //------------- Delete Item---------------
+       app.delete('/products/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await productCollection.deleteOne(query);
+        res.send(result);
+    });
+
     }finally{
 
     }
