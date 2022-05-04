e218cf13d93ce3d26eaf6eac8a5f0bca5705d45f
unknown <mdtauhidulislam509@gmail.com>
2022-05-04 16:43:39 +0600

    total product

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express=require('express')
 const app= express()
 require('dotenv').config()
 var cors=require('cors')
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 
 app.use(cors())
@@ -11,6 +13,26 @@ require('dotenv').config()
 
 const port =process.env.PORT || 5000
 
+
+///-------jwt task---------
+function verifyJWT(req,res,next){
+    const authHeader=req.headers.authorization;
+   
+   if(!authHeader){
+       return res.status(401).send({message:'unauthorized access'})
+   }
+   const token=authHeader.split(' ')[1];
+   jwt.verify(token,process.env.DB_TOKEN_SECRET,(err,decoded)=>{
+       if(err){
+        return res.status(403).send({ message: 'Forbidden access' });
+       }
+   
+    console.log('decoded', decoded);
+   req.decoded = decoded;
+    next();
+})
+
+}
 app.get('/',(req,res)=>{
     res.send('successfully');
 })
@@ -25,18 +47,25 @@ async function run(){
     try{
         await client.connect()
         const productCollection=client.db('assignment11').collection('laptopbrand')
+        
+        //Auth jwt
+        app.post('/login',async(req,res)=>{
+            const user=req.body;
+            const accessToken = jwt.sign(user, process.env.DB_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+    
+    //api all products    
         app.get('/products', async(req,res)=>{
             const query={}
             const abc=productCollection.find(query)
             const result= await abc.toArray();
             res.send(result);
         })
-        app.get('/productCount', async(req,res)=>{
-            const query={}
-             const abc=productCollection.find(query)
-             const count= await abc.count();
-             res.send({count});
-         })
+        
 
          //---------inventory/:id-----------
 
@@ -74,15 +103,20 @@ async function run(){
 
      // item Collection API----------
 
-     app.get('/item',  async (req, res) => {
-        
+     app.get('/item', verifyJWT, async (req, res) => {
+        const decodedEmail = req.decoded.email;
         const email = req.query.email;
-       
-      
+        if (email === decodedEmail){
             const query = {email:email};
             const cursor = productCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
+
+        } else{
+            res.status(403).send({message: 'forbidden access'})
+        }
+      
+            
         
         
     })
@@ -104,6 +138,20 @@ async function run(){
         res.send(result);
 
     })
+
+    ///------------count toatal product--------------
+
+    app.get('/productCount', async(req,res)=>{
+        
+
+        const count = await productCollection.estimatedDocumentCount();
+        res.send({count});
+     })
+
+
+    
+
+    //last finally complete
 
     }finally{
 
